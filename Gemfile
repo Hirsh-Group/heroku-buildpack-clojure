5df6bb943b72d62ebf5d640c01dd18dda43c7014
Eugene Pakhomov <p1himik@gmail.com>
2019-05-01 20:19:39 +0300

    Adapt to HGS needs for deps.edn projects

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem 'heroku_hatchet'
-gem 'rspec-retry'
-gem 'rspec-expectations'
--- lib/common.sh
+++ lib/common.sh
@@ -14,45 +14,6 @@ cache_copy() {
 }
 
 # Install node.js
-install_nodejs() {
-  local version="${1:?}"
-  local dir="${2:?}"
-  local os="linux"
-  local cpu="x64"
-  local platform="$os-$cpu"
-
-  echo "Resolving node version $version..."
-  if ! read number url < <(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/node/$platform/latest.txt"); then
-    local error=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/node/$platform/latest.txt")
-    if [[ $error = "No result" ]]; then
-      echo "Could not find Node version corresponding to version requirement: $version";
-    else
-      echo "Error: Invalid semantic version \"$version\""
-    fi
-  fi
-
-  echo "Downloading and installing node $version..."
-  local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
-  if [ "$code" != "200" ]; then
-    echo "Unable to download node: $code" && false
-  fi
-  tar xzf /tmp/node.tar.gz -C /tmp
-  mv /tmp/node-v$version-$os-$cpu $dir
-  chmod +x $dir/bin/*
-}
-
-detect_and_install_nodejs() {
-  local buildDir=${1}
-  if [ ! -d ${buildDir}/.heroku/nodejs ] && [ "true" != "$SKIP_NODEJS_INSTALL" ]; then
-    if [ "$(grep lein-npm ${buildDir}/project.clj)" != "" ] || [ -n "$NODEJS_VERSION"  ]; then
-      nodejsVersion=${NODEJS_VERSION:-4.2.1}
-      echo "-----> Installing Node.js ${nodejsVersion}..."
-      install_nodejs ${nodejsVersion} ${buildDir}/.heroku/nodejs 2>&1 | sed -u 's/^/       /'
-      export PATH=${buildDir}/.heroku/nodejs/bin:$PATH
-    fi
-  fi
-}
-
 install_jdk() {
   local install_dir=${1}
 
